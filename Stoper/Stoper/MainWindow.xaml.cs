c48aadc2dae641106af586e077bca483b47985de
nemesisevil <wojciech.baranski@interia.pl>
2016-11-28 15:58:57 +0100

    v 0.1
    Programs "Stoper" for to measure time.
    
    -using library System.Timer.Timer
    -using wpf, xaml
    -polish language

---
+++ Stoper/Stoper/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+
+
+namespace Stoper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Czas czas = new Czas();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+
+            czas.Start();
+            Ekran.Content = Convert.ToString("Rozpoczęto Odliczanie");
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            czas.Stop();
+            czas.konwertuj();
+            Ekran.Content = Convert.ToString(czas.hr + " Godzin, " + czas.mn + " Minut, " + czas.sec + " Sekund," + czas.misec+ " Milisekund");
+            //Ekran.Content += czas.timer.ToString();
+        }
+
+        private void Reser_Click(object sender, RoutedEventArgs e)
+        {
+            czas.reset();
+            Ekran.Content = Convert.ToString("Wyczyszczono dane");
+        }
+    }
+}
---
+++ Stoper/Stoper/czas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+
+namespace Stoper 
+{
+    class Czas
+    {
+        private int licznik = 0;   // liczba setnych sekundy
+        private Timer timer = new Timer();
+
+        public int hr, mn, sec, misec;
+
+        public void Start()
+        {
+            timer.Interval = 1; //co ile sie uruchamia
+            timer.Elapsed += new ElapsedEventHandler(wypisz); // co sek wywoluje funkcje
+            timer.Enabled = true;
+            timer.Start();
+
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+
+        }
+
+        private void wypisz(object sender, EventArgs e)
+        {
+            licznik++;
+        }
+
+       public void konwertuj()
+        {
+            var timeSpan = TimeSpan.FromMilliseconds(licznik);
+            hr = timeSpan.Hours;
+            mn = timeSpan.Minutes;
+            sec = timeSpan.Seconds;
+            misec = timeSpan.Milliseconds;
+        }
+
+        public void reset()
+        {
+            licznik = 0;
+            hr = 0;
+            mn = 0;
+            sec = 0;
+            misec = 0;
+
+
+        }
+
+
+    }
+}
